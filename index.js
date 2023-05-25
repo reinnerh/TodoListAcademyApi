a16ff0a94471ae45683098a172a6ea08e20e744c
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-05-25 09:26:43 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,27 @@
+ /* SINCRONIZAÇÂO COM BANDCO DE DADOS */
+
+/* (async () => {
+  const database = require("./db");
+  const Produto = require("./produto");
+
+  try {
+    const resultado = await database.sync();
+    console.log("ok, funcionou");
+    console.log(resultado);
+  } catch (error) {
+    console.log("err" + error);
+  }
+})(); */
+
+/* MODEL - TABELAS  */
+
+/* const todoList = [
+  {
+    id: 1,
+    title: "",
+    
+  }
+]
+const  */
+
+/*  */
