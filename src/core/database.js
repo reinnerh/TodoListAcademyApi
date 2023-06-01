e3c6ac4c26196e3833fc6163db82b5cfc187c54d
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-06-01 13:38:19 -0300

    usa variaveis de ambiente na conexão com banco

--- src/core/database.js
+++ src/core/database.js
@@ -2,7 +2,9 @@ const Sequelize = require("sequelize");
 const { setup } = require("./setup");
 const db = {};
 
-const sequelize = new Sequelize("postgres://user:pass@todo_list_db:5432/db"); // Example for postgres
+const sequelize = new Sequelize(
+  `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@todo_list_db:5432/${process.env.DB_NAME}`
+);
 
 setup(sequelize, db);
 
